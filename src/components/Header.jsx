685089638a00eb1542fdb7bdb934a926572f04ba
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-06-12 15:52:13 -0700

    Cleaned up Header component which no longer needs to receive any Prop but only needs access to the items array within the Context.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
 
 import CartModal from './CartModal.jsx';
+import { CartContext } from '../store/shopping-cart-context.jsx';
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
+export default function Header() {
+  const { items } = useContext(CartContext);
+
   const modal = useRef();
 
-  const cartQuantity = cart.items.length;
+  const cartQuantity = items.length;
 
   function handleOpenCartClick() {
     modal.current.open();
